aa0ed5ae88da4af62bfd6fd6b2114e7c6495253f
mdwheelus92 <michellewheelus@gmail.com>
2023-01-17 20:35:18 -0800

    Added Navbar and basic headings

---
+++ src/Homepage.js
@@ -0,0 +1,52 @@
+import React from "react";
+import Navigation from "./Navigation.js";
+import Mission from "./Mission.js";
+import Contact from "./Contact.js";
+import Services from "./Services.js";
+
+export default function Homepage() {
+  return (
+    <div class="container">
+      <Navigation />
+      <h1 class="pt-5">Lighthouse Church</h1>
+      <h4>Belong. Believe. Become.</h4>
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <Mission />
+      <Contact />
+      <Services class="services" />
+    </div>
+  );
+}
+
+<script
+  src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"
+  integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3"
+  crossorigin="anonymous"
+></script>;
---
+++ src/Services.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Services() {
+  return (
+    <div>
+      <h2>Sunday Service @ 6pm</h2>
+    </div>
+  );
+}
